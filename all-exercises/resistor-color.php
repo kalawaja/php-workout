21a4677e0c3f4eaad3b57b746d70b5c30d6b9f7e
Ramazan İLTER <remzilter@gmail.com>
2022-10-10 02:00:30 +0300

    changes resistor-color.php

--- all-exercises/resistor-color.php
+++ all-exercises/resistor-color.php
@@ -3,8 +3,18 @@
 
 # Resistor Color (Türkçesi: Direnç Rengi)
 
-/*
-Color Encoding of Resistors (Türkçesi: Dirençlerin Renk Kodlaması)
+/* Instructions
+If you want to build something using a Raspberry Pi, you'll probably use resistors. For this exercise, you need to know two things about them:
+
+Each resistor has a resistance value.
+Resistors are small - so small in fact that if you printed the resistance value on them, it would be hard to read.
+To get around this problem, manufacturers print color-coded bands onto the resistors to denote their resistance values. Each band has a position and a numeric value.
+
+The first 2 bands of a resistor have a simple encoding scheme: each color maps to a single number.
+
+In this exercise you are going to create a helpful program so that you don't have to remember the values of the bands.
+
+These colors are encoded as follows:
 
 Black: 0
 Brown: 1
@@ -16,24 +26,42 @@ Blue: 6
 Violet: 7
 Grey: 8
 White: 9
-*/
+The goal of this exercise is to create a way:
 
-const COLORS = array(
-    "black",
-    "brown",
-    "red",
-    "orange",
-    "yellow",
-    "green",
-    "blue",
-    "violet",
-    "grey",
-    "white"
-);
+to look up the numerical value associated with a particular color band
+to list the different band colors */
 
-function colorCode(string $color): int
-{
-    return array_search($color, COLORS);
+function colorCode($color) {
+    $colors = array(
+        "black" => 0,
+        "brown" => 1,
+        "red" => 2,
+        "orange" => 3,
+        "yellow" => 4,
+        "green" => 5,
+        "blue" => 6,
+        "violet" => 7,
+        "grey" => 8,
+        "white" => 9
+    );
+    return $colors[$color];
 }
 
+function colors() {
+    $colors = array(
+        "black",
+        "brown",
+        "red",
+        "orange",
+        "yellow",
+        "green",
+        "blue",
+        "violet",
+        "grey",
+        "white"
+    );
+    return $colors;
+}
+
+colorCode("red");
 echo colorCode("red");
