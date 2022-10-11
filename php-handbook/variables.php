97bd06c965eeb2778b2e396eb95f89e55b8e749c
Ramazan İLTER <remzilter@gmail.com>
2022-10-12 01:09:26 +0300

    commit variables.php

---
+++ php-handbook/variables.php
@@ -0,0 +1,15 @@
+
+<?php
+
+$fName = "Ramazan";
+$lName = 'ilter';
+$age = 32;
+
+/*
+Once a variable is assigned a value, 
+for example a string, 
+we can reassign it a different type of value, 
+like a number:
+*/
+// PHP won’t complain that now the type is different.
+$fName = 23; 
