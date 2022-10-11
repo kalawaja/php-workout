2a78f704cf513e924c9a0a9ae3d543d05a8f8ea6
Ramazan İLTER <remzilter@gmail.com>
2022-10-12 01:46:22 +0300

    commit var-dump.php

---
+++ php-handbook/var-dump.php
@@ -0,0 +1,18 @@
+
+<?php
+
+// Printing the value of a variable
+
+$name = "Ramazan";
+var_dump($name);
+
+// Output: string(7) "Ramazan"
+
+echo "<br>"; // for space between the lines of code in the browser
+
+$age = 32;
+var_dump($age);
+
+// Output: int(32)
+
+# important: var_dump() is one of the essential tools in your PHP debugging toolbelt.
