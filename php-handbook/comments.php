86ac6626628607729f4d9dc745fd333d3e965e2d
Ramazan İLTER <remzilter@gmail.com>
2022-10-12 01:13:51 +0300

    commit comments.php

---
+++ php-handbook/comments.php
@@ -0,0 +1,20 @@
+
+<?php
+
+// single line comment
+
+# single line comment
+
+/* this is a multi line comment (1) */
+
+/*
+multi
+line
+comment (2)
+*/
+
+/*
+ * multi
+ * line
+ * comment (3)
+ */
