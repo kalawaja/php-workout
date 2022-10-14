890026b0c3a2b4389ac6026f69c5aca1187439a8
Ramazan İLTER <remzilter@gmail.com>
2022-10-14 23:50:42 +0300

    update browser-search.php

--- php-handbook/browser-search.php
+++ php-handbook/browser-search.php
@@ -4,3 +4,21 @@
 # Let's detect which browser your visitors are using.
 
 echo $_SERVER['HTTP_USER_AGENT'];
+
+?>
+
+
+<?php
+# Mixing HTML and PHP
+if (strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== FALSE) {
+?>
+<h3>strpos() false olmayan bir değer döndürmelidir</h3>
+<p>Internet Explorer kullanmaktasınız</p>
+<?php
+} else {
+?>
+<h3>strpos() false döndürmelidir</h3>
+<p>Internet Explorer kullanmıyorsunuz</p>
+<?php
+}
+?>
