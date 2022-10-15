645fd52267bb6c8cac28a5cc1f6602fdce1ba0f5
Ramazan İLTER <remzilter@gmail.com>
2022-10-15 17:22:25 +0300

    commit string.php

--- php-handbook/string.php
+++ php-handbook/string.php
@@ -37,7 +37,7 @@ echo 'This will not expand: \n a newline';
 // Outputs: Variables do not $expand $either
 echo 'Variables do not $expand $either';
 ?>
-<!--
+
 # Double quoted
 If the string is enclosed in double-quotes ("), PHP will interpret the following escape sequences for special characters:
     <strong>Escaped characters</strong>
@@ -54,7 +54,7 @@ If the string is enclosed in double-quotes ("), PHP will interpret the following
 
 # Simple syntax
 If a dollar sign ($) is encountered, the parser will greedily take as many tokens as possible to form a valid variable name. Enclose the variable name in curly braces to explicitly specify the end of the name.
--->
+
 <?php
 $juice = "apple";
 
