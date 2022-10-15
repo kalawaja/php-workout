27295ca680ecd8c50cce90911fe7f95ea442239f
Ramazan İLTER <remzilter@gmail.com>
2022-10-15 14:17:58 +0300

    commit bool.php

---
+++ php-handbook/bool.php
@@ -0,0 +1,19 @@
+
+# Booleans
+This is the simplest type. A bool expresses a truth value. It can be either true or false.
+
+<?php
+var_dump((bool) "");        // bool(false)
+var_dump((bool) "0");       // bool(false)
+var_dump((bool) 1);         // bool(true)
+var_dump((bool) -2);        // bool(true)
+var_dump((bool) "foo");     // bool(true)
+var_dump((bool) 2.3e5);     // bool(true)
+var_dump((bool) array(12)); // bool(true)
+var_dump((bool) array());   // bool(false)
+var_dump((bool) "false");   // bool(true)
+
+# Warnings: -1 is considered true, like any other non-zero(whether negative or positive) number.
+var_dump((bool) -1);        // bool(true)
+var_dump((bool) 0);         // bool(false)
+?>
