124dcc5979e9c2efcc25bf5cf64da08a147e3c9b
Ramazan İLTER <remzilter@gmail.com>
2022-10-28 23:45:53 +0300

    add changes with html input

--- all-exercises/average-student.php
+++ all-exercises/average-student.php
@@ -15,7 +15,53 @@ Your points are not included in the array of your class's points. For calculatin
 ?>
 
 <html>
+  <head>
+    <style>
+      body {
+        font-family: monospace;
+        font-size: 1.2em;
+        margin: 10% auto;
+        padding: 10% auto;
+        align-items: center;
+        justify-content: center;
+        text-align: center;
+      }
+      label {
+        display: inline-block;
+        margin-bottom: 10px;
+        font-size: 1.2em;
+        font-weight: bold;
+      }
+      input {
+        display: block;
+        margin: 0 auto;
+        margin-bottom: 10px;
+        padding: 10px;
+        font-size: 1.2em;
+        border: 1px solid #ccc;
+        border-radius: 5px;
+        font-family: monospace;
+      }
+      h1 { color: black; }
+      h2 { color: maroon; }
+      h3 { color: tan; }
+      h4 { color: cadetblue; }
+      h5 { color: orangered; }
+      .p1 { color: green; }
+      .p2 { color: palevioletred; }
+    </style>
+  </head>
   <body>
+    <h1>Average Student</h1>
+    <h2>There was a test in your class and you passed. 
+    <br>  
+    Congratulations!</h2>
+    <h3>But you are an ambitious person. You might want to know if you are better than the average student in your class.</h3>
+    <h4>We have an array of test scores for your class.
+    <br> 
+    We have also calculated your class average.</h4>
+    <h4>Enter your score and compare it to the class average!</h4>
+    <h5>Note: Return True if you are better and False if you are not!</h5>
     <form action="average-student.php" method="post">
       <label for="points">Enter your points:</label>
       <input type="text" name="points" id="points">
@@ -33,13 +79,17 @@ Your points are not included in the array of your class's points. For calculatin
       $average = array_sum($pointsArray) / count($pointsArray);
       // if points are greater than average
       if ($points > $average) {
-        echo "Class average: <strong>$average</strong>. <br>
+        echo "<p class='p1'>" . 
+        "Class average: <strong>$average</strong>. <br>
         You are better than the average student. <br>
-        <strong>Congratulations!</strong> 👍";
+        <strong>Congratulations!</strong> 👍" . 
+        "</p>";
       } else {
-        echo "Class average: <strong>$average</strong>. <br>
+        echo "<p class='p2'>" . 
+        "Class average: <strong>$average</strong>. <br>
         You are not better than the average student. <br>
-        <strong>Never give up!</strong> 👊";
+        <strong>Never give up!</strong> 👊" . 
+        "</p>";
       }
     }
     ?>
