202871f87fe3071b25dd69ee83436510776650e2
Ramazan İLTER <remzilter@gmail.com>
2022-10-29 23:26:52 +0300

    add pure php solution

--- all-exercises/eight-nine-multiplication.php
+++ all-exercises/eight-nine-multiplication.php
@@ -72,3 +72,19 @@
         ?>
     </body>
 </html>
+
+<?php
+# eight-nine-multiplication.php
+
+function simpleMultiplication($number) {
+    if ($number % 2 == 0) {
+        return $number * 8;
+    } else {
+        return $number * 9;
+    }
+}
+
+# Test
+echo simpleMultiplication(2);
+echo simpleMultiplication(3);
+?>
